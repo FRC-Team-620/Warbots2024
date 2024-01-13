fa06b9e7e47c2ff2c22ead5905c9244ea7d10ab1
Luhan Wang <117863446+Lu-han-wang@users.noreply.github.com>
2024-01-12 20:42:10 -0500

    added janky vision tracking

--- src/main/java/org/jmhsrobotics/frc2024/subsystems/vision/VisionSubsystem.java
+++ src/main/java/org/jmhsrobotics/frc2024/subsystems/vision/VisionSubsystem.java
@@ -13,61 +13,73 @@ import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class VisionSubsystem
-        extends SubsystemBase {
-    // load the apriltag field layout
-    AprilTagFieldLayout layout;
+public class VisionSubsystem extends SubsystemBase {
+	// load the apriltag field layout
+	AprilTagFieldLayout layout;
 
-    //declare the camera
-    PhotonCamera cam;
-    
-    // get the camera position on the robot
-    Transform3d camOnRobot = new Transform3d(new Translation3d(0.5, 0, 0.3), new Rotation3d());
+	// declare the camera
+	PhotonCamera cam;
 
-    //construct a photonPoseEstimator
-    PhotonPoseEstimator estimator = new PhotonPoseEstimator(this.layout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, this.camOnRobot);
+	// get the camera position on the robot
+	Transform3d camOnRobot = new Transform3d(new Translation3d(0.5, 0, 0.3), new Rotation3d());
 
-    DriveSubsystem drive;
+	// construct a photonPoseEstimator
+	PhotonPoseEstimator estimator;
 
-    public VisionSubsystem() {
-        this.cam = new PhotonCamera("Arducam_OV9281_USB_Camera");
-        this.drive = new DriveSubsystem();
-        try{
-            AprilTagFieldLayout.loadFromResource(Filesystem.getDeployDirectory().toString() + "2024-crescendo.json");
-        }
-        catch(IOException e){
-            //TODO: error handling
-        }
-    }
+	DriveSubsystem drive;
 
-    @Override
-    public void periodic() {
-        PhotonPipelineResult results = this.cam.getLatestResult();
-        PhotonTrackedTarget target = results.getBestTarget();
-        SmartDashboard.putBoolean("Vision/isConnected", this.cam.isConnected());
-        if (target != null) {
-            Transform3d bestCameraToTarget = target.getBestCameraToTarget();
-            SmartDashboard.putBoolean("Vision/hasTraget", results.hasTargets());
-            SmartDashboard.putNumber("Vision/FiducialID", target.getFiducialId());
-            NT4Util.putPose3d("Vision/target",
-                    new Pose3d(bestCameraToTarget.getTranslation(), bestCameraToTarget.getRotation()));
-        }
+	public VisionSubsystem(DriveSubsystem drive) {
+		this.cam = new PhotonCamera("clarance");
+		this.estimator = new PhotonPoseEstimator(this.layout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, this.cam,
+				this.camOnRobot);
+		this.drive = drive;
 
-        //Puting the estimated pose to the network table
-        NT4Util.putPose3d("Vision/EstimatedTarget",this.getEstimatedGlobalPose(this.drive.getPose()).get().estimatedPose);
-    }
+		System.out.println(Filesystem.getDeployDirectory().getAbsolutePath());
+		try {
+			layout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2024Crescendo.m_resourceFile);
+		} catch (IOException e) {
+			// TODO: error handling
+			System.out.println(e);
+			DriverStation.reportError("Fail to load the april tag map", e.getStackTrace());
+		}
+		estimator.setFieldTags(layout);
+	}
 
-    public Optional<EstimatedRobotPose> getEstimatedGlobalPose(Pose2d prevPose){
-        this.estimator.setReferencePose(prevPose);
-        return this.estimator.update();
-    }
+	@Override
+	public void periodic() {
+		PhotonPipelineResult results = this.cam.getLatestResult();
+		PhotonTrackedTarget target = results.getBestTarget();
+		SmartDashboard.putBoolean("Vision/isConnected", this.cam.isConnected());
+		if (target != null) {
+			Transform3d bestCameraToTarget = target.getBestCameraToTarget();
+			SmartDashboard.putBoolean("Vision/hasTraget", results.hasTargets());
+			SmartDashboard.putNumber("Vision/FiducialID", target.getFiducialId());
+			NT4Util.putPose3d("Vision/target",
+					new Pose3d(bestCameraToTarget.getTranslation(), bestCameraToTarget.getRotation()));
+		}
+
+		// Puting the estimated pose to the network table
+		var estimatedPose = this.getEstimatedGlobalPose(this.drive.getPose());
+		if (estimatedPose.isPresent()) {
+			NT4Util.putPose3d("Vision/EstimatedTarget", estimatedPose.get().estimatedPose);
+		} else {
+			NT4Util.putPose3d("Vision/EstimatedTarget", new Pose3d());
+		}
+	}
+
+	public Optional<EstimatedRobotPose> getEstimatedGlobalPose(Pose2d prevPose) {
+		this.estimator.setReferencePose(prevPose);
+		return this.estimator.update();
+	}
 }
