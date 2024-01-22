c137937e04f5ce2fe8cd907f3622e422ad04443a
m10653 <m10653@users.noreply.github.com>
2024-01-22 18:11:54 -0500

    Add basic Monologue support (#55)

--- src/main/java/org/jmhsrobotics/frc2024/RobotContainer.java
+++ src/main/java/org/jmhsrobotics/frc2024/RobotContainer.java
@@ -27,8 +27,9 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import monologue.Logged;
 
-public class RobotContainer {
+public class RobotContainer implements Logged {
 
 	private ControlBoard control = new CompControl();
 	// Subsystems
