9825b76344acf089c4bca9d68b0b0eb529bc88da
npwtub <145060985+npwtub@users.noreply.github.com>
2024-02-01 18:53:47 -0500

    PID Control for climber
    Added PID control for climber.

--- src/main/java/org/jmhsrobotics/frc2024/subsystems/climber/commands/ClimbCommand.java
+++ src/main/java/org/jmhsrobotics/frc2024/subsystems/climber/commands/ClimbCommand.java
@@ -2,31 +2,52 @@ package org.jmhsrobotics.frc2024.subsystems.climber.commands;
 
 import org.jmhsrobotics.frc2024.subsystems.climber.ClimbSubsystem;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.SparkPIDController;
-
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class ClimbCommand extends Command {
 	private ClimbSubsystem climbSubsystem;
-	private double length;
-	private SparkPIDController lengthPIDController;
+	private double lengthGoal;
+	private ProfiledPIDController climbPID;
+	private Constraints climberConstraints;
 
-	public ClimbCommand(ClimbSubsystem climbSubsystem, double length) {
+	public ClimbCommand(ClimbSubsystem climbSubsystem, double lengthGoal) {
 		this.climbSubsystem = climbSubsystem;
-		this.length = length;
+		this.lengthGoal = lengthGoal;
+		this.climberConstraints = new Constraints(4, 6);
+		// TODO: tune PID vals
+		this.climbPID = new ProfiledPIDController(.1, 0, 0, this.climberConstraints);
+
 		addRequirements(climbSubsystem);
+	}
 
-		this.lengthPIDController = climbSubsystem.getMotor().getPIDController();
-		this.lengthPIDController.setFeedbackDevice(climbSubsystem.getEncoder());
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+		this.climbPID.reset(this.climbSubsystem.getClimbPosition());
+		this.climbPID.setGoal(lengthGoal);
+		this.climbPID.setTolerance(1, 1);
+	}
 
-		// TODO: tune vals
-		lengthPIDController.setP(.1);
-		lengthPIDController.setI(0);
-		lengthPIDController.setD(0);
-		lengthPIDController.setFF(0);
-		lengthPIDController.setOutputRange(-1, 1);
-		lengthPIDController.setReference(length, CANSparkMax.ControlType.kPosition);
+	@Override
+	public void execute() {
+		this.climbPID.setConstraints(climberConstraints);
+		double motorOutput = this.climbPID.calculate(this.climbSubsystem.getClimbPosition());
+		motorOutput = MathUtil.clamp(motorOutput, -1, 1);
+
+		this.climbSubsystem.setMotor(motorOutput);
+
+		SmartDashboard.putNumber("ClimberPID/positionError", this.climbPID.getPositionError());
+		SmartDashboard.putNumber("ClimberPID/velocityError", this.climbPID.getVelocityError());
+	}
+
+	@Override
+	public boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
