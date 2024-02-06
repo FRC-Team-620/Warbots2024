40ff158c504b6bfc0eb97b571f32c4dd48896489
Luhan Wang <117863446+Lu-han-wang@users.noreply.github.com>
2024-02-05 19:18:06 -0500

    added limit switch and follow motor.  TODO finish soft limits.

--- src/main/java/org/jmhsrobotics/frc2024/subsystems/arm/ArmSubsystem.java
+++ src/main/java/org/jmhsrobotics/frc2024/subsystems/arm/ArmSubsystem.java
@@ -7,6 +7,7 @@ import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkAbsoluteEncoder.Type;
+import com.revrobotics.SparkLimitSwitch;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose3d;
@@ -24,14 +25,41 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ArmSubsystem extends SubsystemBase {
 
 	private MechanismLigament2d m_arm;
-	private CANSparkMax armPivot = new CANSparkMax(9, MotorType.kBrushless);
+	private CANSparkMax armPivot = new CANSparkMax(Constants.CAN.kArmPivotRightID, MotorType.kBrushless);
+	private CANSparkMax armHelper = new CANSparkMax(Constants.CAN.kArmPivotFollowerID, MotorType.kBrushless);
 	private SimableAbsoluteEncoder pitchEncoder;
 	private Mechanism2d mech;
+	private SparkLimitSwitch pitchSwitchF;
+	private SparkLimitSwitch pitchSwitchR;
 
 	public ArmSubsystem() {
+		armPivot.restoreFactoryDefaults();
+		armHelper.restoreFactoryDefaults();
+
 		pitchEncoder = new SimableAbsoluteEncoder(armPivot.getAbsoluteEncoder(Type.kDutyCycle));
 		armPivot.setSmartCurrentLimit(40);
 		armPivot.setIdleMode(IdleMode.kBrake);
+
+		// 1 to 25 gearbox to a 9 tooth to 66 sprocket
+		armPivot.getEncoder().setPositionConversionFactor((1.0 / 25.0) * (9.0 / 66.0));
+
+		armHelper.follow(armPivot);
+		pitchEncoder.setPositionConversionFactor(Units.radiansToDegrees(1));
+		armPivot.getEncoder().setPosition(getArmPitch());
+
+		// armPivot.setSoftLimit(SoftLimitDirection.kReverse, 0);
+		// armPivot.setSoftLimit(SoftLimitDirection.kForward, 180);
+		// armPivot.enableSoftLimit(SoftLimitDirection.kForward, true);
+		// armPivot.enableSoftLimit(SoftLimitDirection.kReverse, true);
+
+		pitchSwitchF = armPivot.getForwardLimitSwitch(SparkLimitSwitch.Type.kNormallyOpen);
+		pitchSwitchR = armPivot.getReverseLimitSwitch(SparkLimitSwitch.Type.kNormallyOpen);
+		pitchSwitchF.enableLimitSwitch(true);
+		pitchSwitchR.enableLimitSwitch(true);
+
+		// armPivot.burnFlash();
+		// armHelper.burnFlash();
+
 		init2d();
 		if (RobotBase.isSimulation()) {
 			initSim();
@@ -68,7 +96,9 @@ public class ArmSubsystem extends SubsystemBase {
 				new Pose3d(0.2, 0, 0.283, new Rotation3d(0, Units.degreesToRadians(getArmPitch()), 0)));
 
 	}
+
 	SingleJointedArmSim armSim;
+
 	public void initSim() {
 		double armGearRatio = 183.33;
 		double moi = 0.399649199419221;
@@ -76,6 +106,7 @@ public class ArmSubsystem extends SubsystemBase {
 				Units.degreesToRadians(180), true, 0);
 		// simEncoder = new RevEncoderSimWrapper(null, null);
 	}
+
 	@Override
 	public void simulationPeriodic() {
 		double armVolts = MathUtil.clamp(armPivot.get() * 12, -12, 12);
