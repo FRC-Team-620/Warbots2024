f2ec8c422d45e986038068653549618e54fdb031
Andrei Stan <astan54321@gmail.com>
2024-03-14 20:07:21 -0400

    begin tune module velocity PID, add Choreo testing paths

--- src/main/java/org/jmhsrobotics/frc2024/Constants.java
+++ src/main/java/org/jmhsrobotics/frc2024/Constants.java
@@ -169,9 +169,9 @@ public class Constants {
 		public static final double kTurningEncoderPositionPIDMinInput = 0; // radians
 		public static final double kTurningEncoderPositionPIDMaxInput = kTurningEncoderPositionFactor; // radians
 
-		public static final double kDrivingP = 0.08;
+		public static final double kDrivingP = 0.1;
 		public static final double kDrivingI = 0;
-		public static final double kDrivingD = 0;
+		public static final double kDrivingD = 0.4;
 		public static final double kDrivingFF = 1 / kDriveWheelFreeSpeedRps;
 		public static final double kDrivingMinOutput = -1;
 		public static final double kDrivingMaxOutput = 1;
