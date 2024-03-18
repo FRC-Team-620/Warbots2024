6b820cb6f39af129ec4948ed2535504b66122d88
m10653 <m10653@users.noreply.github.com>
2024-03-18 22:58:10 +0000

    Fix Import Error

--- src/main/java/org/jmhsrobotics/frc2024/subsystems/arm/ArmPIDSubsystem.java
+++ src/main/java/org/jmhsrobotics/frc2024/subsystems/arm/ArmPIDSubsystem.java
@@ -6,6 +6,7 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkAbsoluteEncoder.Type;
 import com.revrobotics.SparkLimitSwitch;
+import com.revrobotics.CANSparkBase.IdleMode;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ProfiledPIDController;
