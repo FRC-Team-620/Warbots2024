cbbd8e198655ea173bf2621be0f79e798a5fa9f5
m10653 <m10653@users.noreply.github.com>
2024-03-19 12:05:02 +0000

    Merge Prep

--- src/main/java/org/jmhsrobotics/frc2024/subsystems/shooter/commands/ShooterAutoCommand.java
+++ src/main/java/org/jmhsrobotics/frc2024/subsystems/shooter/commands/ShooterAutoCommand.java
@@ -9,18 +9,10 @@ public class ShooterAutoCommand extends Command {
 	private ShooterSubsystem shooterSubsystem;
 	private double targetRPM;
 
-	/**
-	 * Spins up shooter to the target rpm. Command never ends, When command is
-	 * exited, spin wheels back to zero rpm
-	 *
-	 * @param shooterSubsystem
-	 * @param targetRPM
-	 */
 	public ShooterAutoCommand(ShooterSubsystem shooterSubsystem, double targetRPM) {
 		this.shooterSubsystem = shooterSubsystem;
 		this.targetRPM = targetRPM;
 	}
-
 	@Override
 	public void execute() {
 		this.shooterSubsystem.set(this.targetRPM, ControlType.PID);
@@ -29,6 +21,10 @@ public class ShooterAutoCommand extends Command {
 	@Override
 	public boolean isFinished() {
 		return this.shooterSubsystem.atGoal();
-		// return false;
+	}
+
+	@Override
+	public void end(boolean interrupted) {
+		this.shooterSubsystem.set(0, ControlType.PID);
 	}
 }
