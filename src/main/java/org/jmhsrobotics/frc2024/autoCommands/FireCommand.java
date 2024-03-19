6994724bd51c8493f30e91e8dcc5e9d8c8dbef5d
m10653 <m10653@users.noreply.github.com>
2024-03-19 12:09:59 +0000

    Revert "Merge Prep"
    This reverts commit cbbd8e198655ea173bf2621be0f79e798a5fa9f5.

--- src/main/java/org/jmhsrobotics/frc2024/autoCommands/FireCommand.java
+++ src/main/java/org/jmhsrobotics/frc2024/autoCommands/FireCommand.java
@@ -11,10 +11,17 @@ public class FireCommand extends SequentialCommandGroup {
 	private IntakeSubsystem intakeSubsystem;
 	private ShooterSubsystem shooterSubsystem;
 
+	/**
+	 * Runs the intake for 0.2 seconds and spins up the shooter flywheels to 5000
+	 * rpm with a timeout of 0.2 seconds Whole command ends is 0.2 seconds
+	 *
+	 * @param intakeSubsystem
+	 * @param shooterSubsystem
+	 */
 	public FireCommand(IntakeSubsystem intakeSubsystem, ShooterSubsystem shooterSubsystem) {
 		this.intakeSubsystem = intakeSubsystem;
 		this.shooterSubsystem = shooterSubsystem;
 		addCommands(new ShooterAutoCommand(this.shooterSubsystem, 5000),
-				new IntakeCommand(1, this.intakeSubsystem, this.shooterSubsystem).withTimeout(0.2));
+				new IntakeCommand(1, this.intakeSubsystem, this.shooterSubsystem).withTimeout(0.5));
 	}
 }
